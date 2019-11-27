a196627b0ed7f72279ec16d7e1da1c38de0a640f
Artur Santiago <artursantiago2001@hotmail.com>
2019-11-27 15:53:13 -0300

    add escopo das funçoes que serão feitas

--- src/main/java/com/projetospring/controller/AlunoController.java
+++ src/main/java/com/projetospring/controller/AlunoController.java
@@ -1,5 +1,72 @@
 package com.projetospring.controller;
 
-public class AlunoController {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetospring.entities.Aluno;
+import com.projetospring.service.AlunoService;
+
+@RestController
+public class AlunoController {
+	
+	@Autowired
+	private AlunoService service;
+	
+	private List<Aluno> listaAlunos = new ArrayList<Aluno>();
+	
+	/**
+	 * 
+	 */
+	@GetMapping()
+	public List<Aluno> findAll() {
+		return service.findAll();
+	}
+	
+	/*
+	
+	@GetMapping()
+	public Aluno findAlunoByMatricula() { 
+		return service.getAlunoByMatricula();
+		
+		Função para achar o aluno quando clicar no botão de editar
+		Provavelmete esse aluno será armazenado no localStorage em json
+		
+	}
+	
+	*/
+	
+	/*
+	  
+	@PutMapping()
+	
+		função para salvar um aluno que está sendo atualizado
+		
+	*/
+	
+	/*
+	 @DeleteMapping()
+	 
+	 	função para deletar um aluno quando clicado no botão de deletar
+	 */
+	
+	/*
+	 Função para chamar a pagina de cadastro
+	 */
+	
+	/*
+	 função para chamar a página de listagem
+	 */
 }
+
+
+
+
+
+
+
+
+
